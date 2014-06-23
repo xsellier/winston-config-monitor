d44235f80e061bd33a9c24d9a9d47ac65c2b5c8b
xsellier <xsellier@can2go.com>
2014-06-23 11:56:03 -0400

    Complete README.md file Add dev dependencies Add unit tests

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-winston-config-monitor
-======================
+# winston-config-monitor [![Build Status](https://travis-ci.org/binogure/winston-config-monitor.png?branch=master)](http://travis-ci.org/binogure/winston-config-monitor) [![Dependency Status](https://gemnasium.com/binogure/winston-config-monitor.png)](https://gemnasium.com/binogure/winston-config-monitor) [![NPM version](https://badge.fury.io/js/winston-config-monitor.png)](http://badge.fury.io/js/winston-config-monitor) [![Coverage Status](https://coveralls.io/repos/binogure/winston-config-monitor/badge.png)](https://coveralls.io/r/binogure/winston-config-monitor)
 
-  A helper to easily configure winston with reload.
+## Motivation
+[winston](https://github.com/flatiron/winston) is designed to be a simple and universal logging library with support for multiple transports.
+But, imho, it misses a full logger reloader on configuration change.
---
+++ test/loadFiles.js
@@ -0,0 +1,96 @@
+var fs                   = require('fs')
+  , winstonConfigMonitor = require('../lib/winston-config-monitor')
+  , chai                 = require('chai')
+  , _                    = require('lodash')
+  , sinon                = require('sinon')
+  , spies                = require('chai-spies');
+
+chai.use(spies);
+
+var should          = chai.should()
+  , expect          = chai.expect
+  , logger          = winstonConfigMonitor.getLogger()
+  , spyLoggerAdd    = chai.spy(logger.add)
+  , spyLoggerRemove = chai.spy(logger.remove);
+
+logger.add          = spyLoggerAdd;
+logger.remove       = spyLoggerRemove;
+
+describe("The winston-config-monitor", function() {
+
+  beforeEach(function(done) {
+    winstonConfigMonitor.clear();
+    done();
+  });
+
+  it("should load a well formed config file correctly", function(done) {
+    var configFile      = "test/mock/winston-options-valid.json";
+
+    winstonConfigMonitor.add(logger.transports.Console, configFile, "loggersOptions.console");
+    winstonConfigMonitor.add(logger.transports.File, configFile, "loggersOptions.file");
+
+    fs.readFile(configFile, function(err, data) {
+      var fileContent = JSON.parse(data);
+
+      fileContent.loggersOptions.console.level = "debug";
+
+      fs.writeFile(configFile, JSON.stringify(fileContent, null, 2), function(err) {
+        expect(err).to.not.exist;
+
+        // 4 times because there is many steps when we write a whole file with node fs.
+        // And we count the 2 adds from the beginning.
+        expect(spyLoggerAdd).to.have.been.called.exactly(6);
+        expect(spyLoggerRemove).to.have.been.called.exactly(4);
+
+        setTimeout(function() {
+          fileContent.loggersOptions.console.level = "info";
+          fs.writeFile(configFile, JSON.stringify(fileContent, null, 2), function(err) {
+
+            // 2 + 4 + 4 = 10, and 4 + 4 = 8 for remove
+            expect(spyLoggerAdd).to.have.been.called.exactly(10);
+            expect(spyLoggerRemove).to.have.been.called.exactly(8);
+
+            expect(fs.existsSync(fileContent.loggersOptions.file.filename)).to.be.true;
+            fs.unlinkSync(fileContent.loggersOptions.file.filename);
+
+            done();
+          });
+        }, 1000);
+      });
+    });
+  });
+
+  it("shouldn't load a bad formed config file", function(done) {
+    var configFile = "test/mock/winston-options-invalid.json";
+    var addConsole = function() {
+      winstonConfigMonitor.add(logger.transports.Console, configFile, "loggersOptions.console");
+    };
+
+    var addFile = function() {
+      winstonConfigMonitor.add(logger.transports.File, configFile, "loggersOptions.file");
+    };
+    
+    expect(addConsole).to.throw(/Could not parse JSON file at file path/);
+    expect(addFile).to.throw(/Could not parse JSON file at file path/);
+      
+    done();
+  });
+
+  it("shouldn't load a well formed config file with an invalid logger options", function(done) {
+    var configFile = "test/mock/winston-options-valid.json";
+    var addConsole = function() {
+      winstonConfigMonitor.add(logger.transports.Console, configFile, "loggersOptions.Console");
+    };
+
+    var addFile = function() {
+      winstonConfigMonitor.add(logger.transports.File, configFile, "loggersOptions.unknown");
+    };
+    
+    expect(addConsole).to.throw(/Loggers options are invalid/);
+    expect(addFile).to.throw(/Loggers options are invalid/);
+      
+    done();
+  });
+});
+
+  
